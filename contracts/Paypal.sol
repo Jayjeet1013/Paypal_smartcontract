33f5b33bf522a6d0473b19938cdde454d5606707
Jayjeet1013 <jayjeet1013@gmail.com>
2023-04-05 23:09:43 +0530

    update

---
+++ contracts/Paypal.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract  Paypal {
+
+// Define owner of the smart contract
+
+address public owner;
+construtor (){
+        owner= msg.sender;
+}
+
+//create struct and mapping 
+}
