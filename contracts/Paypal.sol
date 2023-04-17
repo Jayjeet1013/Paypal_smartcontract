af9ffc5c1d061cd2cdebad78dd9ee32ab6677abf
Jayjeet1013 <jayjeet1013@gmail.com>
2023-04-17 18:38:34 +0530

    update smart contract

--- contracts/Paypal.sol
+++ contracts/Paypal.sol
@@ -12,4 +12,30 @@ constructor(){
 }
 
 //create struct and mapping 
+
+struct request{
+        address requestor;
+        uint256 amount;
+        string message;
+        string name;
+}
+
+struct sendReceive{
+        string action;
+        uint256 amount;
+        string message;
+        address otherPartyAddress;
+        string otherPartyName;
+}
+
+struct userName{
+        string name;
+        bool hasName;
+}
+
+mapping(address =>userName) names;
+mapping(address => request[])requests;
+mapping(address => sendReceive[]) history;
+
+
 }
