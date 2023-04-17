2716786f1ba5d1c1d0106e60a961ef42f43b1ca0
Jayjeet1013 <jayjeet1013@gmail.com>
2023-04-17 19:29:45 +0530

    completed smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
+const dotenv = require("dotenv")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
